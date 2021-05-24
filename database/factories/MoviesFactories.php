9806d03326130f884855f172457dcde39be38a7a
fazioandrea.f <fazioandrea.f@gmail.com>
2021-05-24 15:55:33 +0200

    ucfirst

--- database/factories/MoviesFactories.php
+++ database/factories/MoviesFactories.php
@@ -9,8 +9,8 @@ $factory->define(Movies::class, function (Faker $faker) {
     $tmp= $faker-> word;
 
     return [
-        'title'=> $tmp,
-        'original_title'=> $tmp,
+        'title'=> ucfirst ($tmp),
+        'original_title'=> ucfirst ($tmp),
         'nationality'=> $faker -> state,
         'date'=> $faker -> date,
         'vote'=> $faker -> numberBetween(1,1000)*0.01,
